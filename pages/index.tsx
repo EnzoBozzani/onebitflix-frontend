ad9dc116948687c4f269a328dfeb969d5398fa7b
Enzo Bozzani <bozzanienzo@gmail.com>
2023-09-07 00:54:46 -0300

    feat: configured directories and sass variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
-import Head from 'next/head';
-import styles from '@/styles/HomeNoAuth.module.scss';
+import Head from "next/head";
+import styles from "../styles/HomeNoAuth.module.scss"
 
-//teremos páginas autenticadas e outras não autenticadas, 
-//como essa página
-const HomeNoAuth = () => {
+const HomeNotAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."/>
       </Head>
-      <main>
-
-      </main>
+      <main></main>
     </>
   )
 };
 
-export default HomeNoAuth;
+export default HomeNotAuth;
 
